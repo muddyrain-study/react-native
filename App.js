0e1a264910dbb083de41ad1774292b679b45a2ff
“muddyrain” <qiu3291002845@gmail.com>
2023-06-21 13:55:53 +0800

    feat: 2-3容器组件

--- App.js
+++ App.js
@@ -1,29 +1,76 @@
 import React, { useState } from 'react';
-import { View, Switch, StyleSheet } from 'react-native';
+import {
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableNativeFeedback,
+  TouchableHighlight,
+  TouchableWithoutFeedback,
+  Text,
+} from 'react-native';
 
-const App = () => {
-  const [isEnabled, setIsEnabled] = useState(false);
-  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+export default function App() {
+  const [count, setCount] = useState(0);
 
   return (
     <View style={styles.container}>
-      <Switch
-        trackColor={{ false: '#767577', true: '#81b0ff' }}
-        thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
-        ios_backgroundColor='#3e3e3e'
-        onValueChange={toggleSwitch}
-        value={isEnabled}
-      />
+      <TouchableHighlight
+        style={styles.touchableStyle}
+        onPress={() => setCount(count + 1)}
+      >
+        <Text style={styles.txtStyle}>点击加1</Text>
+      </TouchableHighlight>
+
+      <TouchableOpacity
+        style={styles.touchableStyle}
+        onPress={() => setCount(count + 1)}
+      >
+        <Text style={styles.txtStyle}>点击加1</Text>
+      </TouchableOpacity>
+
+      <TouchableWithoutFeedback onPress={() => setCount(count + 1)}>
+        <View style={styles.touchableStyle}>
+          <Text style={styles.txtStyle}>点击加1</Text>
+        </View>
+      </TouchableWithoutFeedback>
+
+      <TouchableNativeFeedback onPress={() => setCount(count + 1)}>
+        <View style={styles.touchableStyle}>
+          <Text style={styles.txtStyle}>点击加1</Text>
+        </View>
+      </TouchableNativeFeedback>
+
+      <Text style={[styles.countText]}>{count !== 0 ? count : null}</Text>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  touchableStyle: {
+    width: 300,
+    height: 38,
+    borderRadius: 5,
+    alignSelf: 'center',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#06C1AE',
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  txtStyle: {
+    color: '#ffffff',
+    textAlign: 'center',
+    fontSize: 18,
+  },
+  countText: {
+    marginTop: 10,
+    alignSelf: 'center',
+    fontSize: 38,
+    color: '#06C1AE',
   },
 });
-
-export default App;
