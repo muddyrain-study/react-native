3b3fe2da33516aec234f24a4f90c7dc7b6b08456
“muddyrain” <qiu3291002845@gmail.com>
2023-10-07 14:29:21 +0800

    feat: drawer and tab navigater

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { NavigationContainer } from '@react-navigation/native';
+import 'react-native-gesture-handler';
+import { NavigationContainer, useRoute } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
+import { Ionicons } from '@expo/vector-icons';
 import HomeScreen from './views/HomeScreen';
 import SettingScreen from './views/SettingScreen';
 import ProfileScreen from './views/ProfileScreen';
 import DetailScreen from './views/DetailScreen';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { Image, Text, View } from 'react-native';
 
 // 创建一个堆栈导航器
@@ -13,7 +15,8 @@ const HomeStack = createNativeStackNavigator();
 const SettingStack = createNativeStackNavigator();
 // 创建一个底部导航器
 const Tab = createBottomTabNavigator();
-
+// 创建一个抽屉导航器
+const Drawer = createDrawerNavigator();
 function LogoTitle() {
   return (
     <View style={{ backgroundColor: 'blue', width: 375 }}>
@@ -29,8 +32,28 @@ function LogoTitle() {
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={{ headerShown: false }}>
-        <Tab.Screen name='First'>
+      {/* <Tab.Navigator
+        screenOptions={({ route }) => ({
+          headerShown: false,
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName = '';
+            if (route.name === 'First') {
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+            } else if (route.name === 'Second') {
+              iconName = focused ? 'ios-mail' : 'ios-mail-unread';
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+      >
+        <Tab.Screen
+          name='First'
+          options={{
+            tabBarBadge: 0,
+          }}
+        >
           {() => (
             <HomeStack.Navigator>
               <HomeStack.Screen name='Home' component={HomeScreen} />
@@ -46,7 +69,15 @@ export default function App() {
             </SettingStack.Navigator>
           )}
         </Tab.Screen>
-      </Tab.Navigator>
+      </Tab.Navigator> */}
+      <Drawer.Navigator initialRouteName='主页'>
+        <Drawer.Screen name='主页' component={HomeScreen} />
+        <Drawer.Screen
+          name='详情页'
+          initialParams={{ id: 1, name: '吊毛', age: 18 }}
+          component={DetailScreen}
+        />
+      </Drawer.Navigator>
       {/* <Stack.Navigator
         screenOptions={{
           headerStyle: {
--- views/HomeScreen.js
+++ views/HomeScreen.js
@@ -13,6 +13,31 @@ export default HomeScreen = ({ navigation, route }) => {
       <Text>这是首页</Text>
       <Text>{route.params?.post}</Text>
       <Button title='跳转到 detail 页面' onPress={pressHandle} />
+      <Button
+        title='跳转到Second页面'
+        color={'#f194ff'}
+        onPress={() => {
+          navigation.navigate('Profile');
+        }}
+      />
+      <Button
+        title='打开抽屉'
+        onPress={() => {
+          navigation.openDrawer();
+        }}
+      />
+      <Button
+        title='关闭抽屉'
+        onPress={() => {
+          navigation.closeDrawer();
+        }}
+      />
+      <Button
+        title='切换抽屉'
+        onPress={() => {
+          navigation.toggleDrawer();
+        }}
+      />
     </View>
   );
 };
